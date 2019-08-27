ec8ae200aba9467ce664c58eed3a426bba006604
邱昊天 <841965336@qq.com>
2019-08-27 11:07:21 +0800

    ggg

---
+++ tes/src/test/java/test/HahaGit.java
@@ -0,0 +1,14 @@
+package test;
+
+public class HahaGit {
+
+	public HahaGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hahaah");
+	}
+
+}
